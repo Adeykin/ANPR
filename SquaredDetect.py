ea6950865939f347b0d67a78db5eed3bdeb7a0fc
adeykin <adeykin90@gmail.com>
2019-01-08 21:50:10 +0300

    dumping warps

--- SquaredDetect.py
+++ SquaredDetect.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import itertools
 
 def angle_cos(p0, p1, p2):
     d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
@@ -7,6 +8,25 @@ def angle_cos(p0, p1, p2):
     
 def distance(p0, p1):
     return np.sqrt( (p0[0]-p1[0])**2 + (p0[1]-p1[1])**2)
+    
+def order_points_old(pts):
+	rect = np.zeros((4, 2), dtype="int32")
+ 
+	# the top-left point will have the smallest sum, whereas
+	# the bottom-right point will have the largest sum
+	s = pts.sum(axis=1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+ 
+	# now, compute the difference between the points, the
+	# top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
+	diff = np.diff(pts, axis=1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+ 
+	# return the ordered coordinates
+	return rect
     
 class Detector:
     def __init__(self):
@@ -41,7 +61,7 @@ class Detector:
                         cnt = cnt.reshape(-1, 2)
                         max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in range(4)])
                         if max_cos < self.maxCos:
-                            squares.append(cnt)
+                            squares.append(order_points_old(cnt))
         return squares
 
     def detect(self, img):
--- demo.py
+++ demo.py
@@ -1,6 +1,27 @@
 import SquaredDetect
 import sys, os
 import cv2
+import numpy as np
+
+
+# 50x253
+def dumpRects(img, rects, path):
+    if not path:
+        return
+        
+    for rect in rects:
+        print 'dump rect: ' + str(rect)
+        srcTri = np.float32([rect[0], rect[1], rect[3]])
+        dstTri = np.float32([(0, 0), (253, 0), (0, 50) ])
+        print 'srcTri: ' + str(srcTri)
+        print 'dstTri: ' + str(dstTri)
+        warpMat = cv2.getAffineTransform( srcTri, dstTri );
+        warped = cv2.warpAffine( img, warpMat, (253,50) );
+        cv2.imshow('warp', warped)
+        cv2.waitKey()
+
+        
+    
 
 def process(img):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY);
@@ -14,27 +35,34 @@ def process(img):
     
     cv2.imshow('img', img)
     cv2.waitKey()
+    
+    return rects
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print 'python demo.py <imageOrVideoPath>'
     path = sys.argv[1]
     print 'reading: ' + path
     
+    outputPath = None
+    if len(sys.argv) == 3:
+        outputPath = sys.argv[2]        
+    
     if os.path.isfile(path):
         vidcap = cv2.VideoCapture(path)
-        if vidcap:
+        if vidcap: #video
             success,image = vidcap.read()
             while success:
                 process(image)
                 success,image = vidcap.read()
-        else:
+        else:      #image
             img = cv2.imread(path)    
             process(img)
-    elif os.path.isdir(path):
+    elif os.path.isdir(path): #directory
         for f in os.listdir(path):
             print 'try to read: ' + f
             img = cv2.imread(path + '/' + f)  
-            process(img)
+            rects = process(img)
+            dumpRects(img, rects, outputPath)
 

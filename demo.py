1300d6531e6e36a0e80739dea9fd8811c66268ca
adeykin <adeykin90@gmail.com>
2019-01-08 15:09:48 +0300

    may read videos and directories

--- demo.py
+++ demo.py
@@ -1,14 +1,8 @@
 import SquaredDetect
-import sys
+import sys, os
 import cv2
 
-if __name__ == '__main__':
-    #if len(sys.argv) != 2:
-    #print 'python demo.py <imageOrVideoPath>'
-    path = sys.argv[1]
-    #print('reading: ' + path)
-    
-    img = cv2.imread(path)
+def process(img):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY);
     
     detector = SquaredDetect.Detector()
@@ -18,6 +12,29 @@ if __name__ == '__main__':
         print a
         cv2.polylines(img, rects, True, (0,0,255))
     
-    
     cv2.imshow('img', img)
     cv2.waitKey()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print 'python demo.py <imageOrVideoPath>'
+    path = sys.argv[1]
+    print 'reading: ' + path
+    
+    if os.path.isfile(path):
+        vidcap = cv2.VideoCapture(path)
+        if vidcap:
+            success,image = vidcap.read()
+            while success:
+                process(image)
+                success,image = vidcap.read()
+        else:
+            img = cv2.imread(path)    
+            process(img)
+    elif os.path.isdir(path):
+        for f in os.listdir(path):
+            print 'try to read: ' + f
+            img = cv2.imread(path + '/' + f)  
+            process(img)
+
